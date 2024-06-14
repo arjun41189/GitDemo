c34f3013b7f3809ea002c5ad5c46323ad1be3106
arjun <arjun41189@gmail.com>
2024-06-14 15:56:11 +0530

    first commit

---
+++ MyFirstJavaProject/src/test/MyClass.java
@@ -0,0 +1,20 @@
+package test;
+
+
+void display()
+{
+
+	System.out.println("a=" +a);
+}
+
+
+public class MyClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		MyClass2 ob = new MyClass2();
+		ob.display();
+	}
+
+}
---
+++ RCVDemo/src/DemoAutomation.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DemoAutomation 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//ChromeDriver driver = new ChromeDriver();
+		//driver.get("http://www.google.com");
+		
+		//FirefoxDriver driver = new FirefoxDriver();
+		//driver.get("http://www.google.com");
+		
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("http://www.google.com");
+		
+		
+	}
+
+}
